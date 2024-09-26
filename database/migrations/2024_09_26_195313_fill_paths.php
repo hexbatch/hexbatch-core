f985e06283c65fba3b3a08dc98411a01d8897d3a
Will Woodlief <willwoodlief@gmail.com>
2024-09-26 16:12:30 -0500

    Added more columns to paths to meet min requirements

--- database/migrations/2024_09_26_195313_fill_paths.php
+++ database/migrations/2024_09_26_195313_fill_paths.php
@@ -46,12 +46,26 @@ return new class extends Migration
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
 
+            $table->foreignId('path_element_set_id')
+                ->nullable()->default(null)
+                ->comment("The attribute the path part may be about")
+                ->index('idx_path_element_set_id')
+                ->constrained('element_sets')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
             $table->uuid('ref_uuid')
                 ->unique()
                 ->nullable(false)
                 ->comment("used for display and id outside the code");
 
             $table->timestamps();
+
+            $table->integer('path_min_gap')->nullable()->default(null)
+                ->comment("The min number of relationships that must exist between here and there");
+
+            $table->integer('path_max_gap')->nullable()->default(null)
+                ->comment("How max number of relationships must exist between here and there");
         });
 
         DB::statement('ALTER TABLE paths ALTER COLUMN ref_uuid SET DEFAULT uuid_generate_v4();');
@@ -64,6 +78,15 @@ return new class extends Migration
         ");
 
         DB::statement("ALTER TABLE paths Add COLUMN path_logic type_of_child_logic NOT NULL default 'and';");
+
+        DB::statement("CREATE TYPE path_relationship_type AS ENUM (
+            'no_relationship',
+            'shares_type',
+            'in_childish',
+            'in_linkish'
+            );");
+
+        DB::statement("ALTER TABLE paths Add COLUMN path_relationship path_relationship_type NOT NULL default 'no_relationship';");
 
         Schema::table('paths', function (Blueprint $table) {
 
@@ -91,18 +114,24 @@ return new class extends Migration
             $table->dropForeign(['parent_path_id']);
             $table->dropForeign(['path_type_id']);
             $table->dropForeign(['path_attribute_id']);
+            $table->dropForeign(['path_element_set_id']);
 
             $table->dropColumn('path_owner_id');
             $table->dropColumn('parent_path_id');
             $table->dropColumn('path_type_id');
             $table->dropColumn('path_attribute_id');
+            $table->dropColumn('path_element_set_id');
 
             $table->dropColumn('ref_uuid');
+            $table->dropColumn('path_min_gap');
+            $table->dropColumn('path_max_gap');
             $table->dropColumn('created_at');
             $table->dropColumn('updated_at');
             $table->dropColumn('path_logic');
+            $table->dropColumn('path_relationship');
             $table->dropColumn('path_attribute_json_path');
             $table->dropColumn('path_part_name');
         });
+        DB::statement("DROP TYPE path_relationship_type;");
     }
 };
