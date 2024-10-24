ebf88ede42e7c7fd2c86d756f20012bdca0313b6
Will Woodlief <willwoodlief@gmail.com>
2024-10-24 18:32:16 -0500

    All the php documentation tools are horrible for me
    Not dealing with it, will handwrite docs later.
    For now can use the comment section in the docblocks and link there and search in editor

--- app/Sys/Res/Types/Stk/Root/Act/Cmd/TypePublish.php
+++ app/Sys/Res/Types/Stk/Root/Act/Cmd/TypePublish.php
@@ -7,6 +7,13 @@ use App\Sys\Res\Namespaces\Stock\ThisServerNamespace;
 use App\Sys\Res\Types\Stk\Root\Act;
 
 
+
+/**
+ * Publishes the type, any referenced parent types, parent attributes, live rules, live requirements
+ * are given the event of TypePublished and all must agree
+ *
+ * @see \App\Sys\Res\Types\Stk\Root\Evt\Server\TypePublished
+ */
 class TypePublish extends Act\Cmd
 {
     const UUID = 'af28da1b-b148-4cbf-a53f-ccaf641373ea';
